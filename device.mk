42ba98fc5459fdc4bbd6e4d6ccfad3e054fdbbab
Jeferson <jroliveira.oliveira301@gmail.com>
2021-07-09 12:22:38 +0000

    beckham: Build motocamera
    Change-Id: I451a6ca2f70c34113507917a806fde01fcb3e8d9

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/whitelist_modservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/whitelist_modservice.xml
 
+# Moto Camera 2
+PRODUCT_PACKAGES += \
+    MotCamera2
+
 # Motorola Health
 PRODUCT_PACKAGES += \
     motorola.hardware.health@1.0.vendor
